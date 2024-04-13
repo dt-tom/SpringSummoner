805bdc84cacf92feca8d656d72b12ce4d2e7da2d
tu21897 <tunguyen2189721897@yahoo.com>
2024-04-13 11:36:22 -0700

    ui changes

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 // Ingame map dimensions
-export const mapHeight = 4096
-export const mapWidth = 4096
+export const mapHeight = 4096;
+export const mapWidth = 4096;
 
 // On-webpage canvas dimensions
-export const canvasWidth = 800
-export const canvasHeight = 600
+export const canvasWidth = 1280;
+export const canvasHeight = 960;
 
 export const bugMovespeed = 75;
 export const bushSlow = 0.25;
