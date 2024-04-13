0bf282f1b3e600cfd2cc07ec0437b931c873094b
twistLuminary <thomasjtwist@gmail.com>
2024-04-12 23:39:24 -0400

    enemies move towards you

--- src/main.js
+++ src/main.js
@@ -9,19 +9,21 @@ class Example extends Phaser.Scene
     {
         this.load.image('bg', 'assets/sky.png');
         this.load.image('block', 'assets/star.png');
+        this.load.image('enemy', 'assets/bomb.png');
     }
+
+  
 
     create ()
     {
+        // consts
+        const NUMBER_OF_ENEMIES = 10;
         //  Set the camera and physics bounds to be the size of 4x4 bg images
         this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2);
         this.physics.world.setBounds(0, 0, 1920 * 2, 1080 * 2);
 
         //  Mash 4 images together to create our background
         this.add.image(0, 0, 'bg').setOrigin(0);
-        this.add.image(1920, 0, 'bg').setOrigin(0).setFlipX(true);
-        this.add.image(0, 1080, 'bg').setOrigin(0).setFlipY(true);
-        this.add.image(1920, 1080, 'bg').setOrigin(0).setFlipX(true).setFlipY(true);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -30,6 +32,18 @@ class Example extends Phaser.Scene
         this.player.setCollideWorldBounds(true);
 
         this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+
+
+        // make enemies
+        this.enemies = this.physics.add.group();
+
+        for(let i = 0; i < NUMBER_OF_ENEMIES; i++) 
+        {
+            this.enemies.create(Math.random() * 400, Math.random() * 400, 'enemy');
+        }
+
+        this.physics.add.collider(this.player, this.enemies);
+        this.physics.add.collider(this.enemies, this.enemies);  
     }
 
     update ()
@@ -53,6 +67,15 @@ class Example extends Phaser.Scene
         {
             this.player.setVelocityY(500);
         }
+
+    
+        for(const member of this.enemies.getChildren())
+        {
+            //this.physics.moveTo(member, Math.random() * 10, Math.random() * 10);
+            this.physics.moveTo(member, this.player.x + Math.random() * 100, this.player.y + Math.random() * 100, 150)
+        }
+
+
     }
 }
 
